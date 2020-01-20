62c8a75516ba3aacb3fa67e0d2dbd76bd9839338
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-01-20 16:16:28 +0100

    Banale: pos e replace funzionano come case-insensitive se anticipate da una "i"

--- index.php
+++ index.php
@@ -5,20 +5,19 @@ $hello = 'Hello World!';
 // creo una variabile che contenga testo
 $lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero architecto accusamus facere culpa excepturi, ratione perspiciatis minus illum quia ipsam natus ad mollitia, asperiores aliquid quasi. Est eius ea quisquam quod, illum tempore sint? Magni dolores, enim atque, itaque ullam, sunt officia eligendi ad tempora magnam, laborum similique! Totam commodi ea vitae, rem suscipit fugit nesciunt voluptatibus mollitia quod fugiat.';
 
-// rendo tutto in lowercase
-$lower_lorem = strtolower($lorem);
-
-// creo la varibile per il GET
+// creo la variabile per il GET
 $censured = $_GET['badword'];
 
 // devo usare in sincronia str_replace e strpos
-$find = strpos($lower_lorem, $censured);
+
+// quella 'i' in stripos sta per 'Insensitive', quello che ci serviva
+$find = stripos($lorem, $censured);
 
 // se strpos mi ritorna una posizione, ha trovato una corrispondenza, quindi risulta vera
-
 if ($find !== false) {
-    $lorem_censured = str_replace($censured, '***', $lower_lorem);
-    echo $lorem_censured;
+    // stesso discorso per str_ireplace --> così diventa case-insensitive
+    $lorem = str_ireplace($censured, '***', $lorem);
+    echo $lorem;
 } else {
     echo $lorem;
 }
@@ -29,4 +28,3 @@ ossia visualizzare a schermo un paragrafo con all'interno il testo
 in cui le occorrenze della badword sono sostituite da tre "*".
 */
 ?>
-<!-- <h1><?php echo $hello ?></h1> -->
